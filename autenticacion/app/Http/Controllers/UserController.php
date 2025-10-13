b959085db6d5f3415053ac3e3563b750205cb13b
IsaMesa <ismesa@unal.edu.co>
2025-10-13 14:56:59 -0500

    feat: Se agrega Sanctum y proceso de inicio sesion usuarios

---
+++ autenticacion/app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function create_user(Request $request)
+    {
+        $user = new User;
+        $user ->name = $request -> name;
+        $user ->email = $request -> email;
+        $user ->password = bcrypt($request->password); 
+        $user->save();
+        return response()->json($user, 201);
+    }
+
+    public function login(Request $request){
+        $user = User :: where('email', $request->email)->first();
+        if(! $user || ! \Hash::check($request->password, $user->password)){
+            return response()->json(['message' => 'Credenciales invalidas'], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'user_name' => $user->name
+        ]);
+    }
+}
--- autenticacion/routes/api.php
+++ autenticacion/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/create_user', [UserController::class,'create_user']);
+Route::post('/login', [UserController::class,'login']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [UserController::class,'logout']);
+    Route::post('/change_password', [UserController::class,'change_password']);
 });
